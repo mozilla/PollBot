d95c35bfb1b815086fc9d0f48e48e98f1ff1f7fb
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-08-07 19:28:45 +0200

    Handle other error responses.

--- pollbot/middlewares.py
+++ pollbot/middlewares.py
@@ -23,25 +23,27 @@ def error_pages(overrides):
             except web.HTTPException as ex:
                 override = overrides.get(ex.status)
                 if override is None:
-                    raise
+                    return await handle_any(request, ex)
                 else:
                     return await override(request, ex)
             except Exception as ex:
-                override = overrides.get(500)
-                if override is None:
-                    raise
-                else:
-                    return await override(request, error=ex)
+                return await handle_500(request, error=ex)
         return middleware_handler
     return middleware
 
 
+async def handle_any(request, response):
+    return web.json_response({
+        "status": response.status,
+        "message": response.reason
+    }, status=response.status)
+
+
 async def handle_404(request, response):
-    if response.headers['Content-Type'] != "application/json":
-        return web.json_response({
-            "status": 404,
-            "message": "Page '{}' not found".format(request.path)
-        }, status=404)
+    return web.json_response({
+        "status": 404,
+        "message": "Page '{}' not found".format(request.path)
+    }, status=404)
 
 
 async def handle_500(request, response=None, error=None):
