144cc15268027681e3713931a4513a30ae32f2fb
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-08-07 16:23:39 +0200

    Add the product-details tasks.

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,3 +1,5 @@
+import json
+
 import aiohttp
 import asynctest
 import pytest
@@ -7,6 +9,7 @@ from aioresponses import aioresponses
 from pollbot.exceptions import TaskError
 from pollbot.tasks.archives import archives_published
 from pollbot.tasks.bedrock import release_notes_published, security_advisories_published
+from pollbot.tasks.product_details import product_details_published
 
 
 class DeliveryTasksTest(asynctest.TestCase):
@@ -46,31 +49,53 @@ class DeliveryTasksTest(asynctest.TestCase):
         received = await archives_published('firefox', '52.0.2')
         assert received is False
 
-    async def test_security_advisory_tasks_returns_true_if_version_matches(self):
+    async def test_security_advisories_tasks_returns_true_if_version_matches(self):
         url = 'https://www.mozilla.org/en-US/security/known-vulnerabilities/firefox/'
         self.mocked.get(url, status=200, body='<html data-latest-firefox="52.0.2"></html>')
 
         received = await security_advisories_published('firefox', '52.0.2')
         assert received is True
 
-    async def test_security_advisory_tasks_returns_true_if_older_version(self):
+    async def test_security_advisories_tasks_returns_true_if_older_version(self):
         url = 'https://www.mozilla.org/en-US/security/known-vulnerabilities/firefox/'
         self.mocked.get(url, status=200, body='<html data-latest-firefox="52.0.2"></html>')
 
         received = await security_advisories_published('firefox', '52.0')
         assert received is True
 
-    async def test_security_advisory_tasks_returns_false_if_newer_version(self):
+    async def test_security_advisories_tasks_returns_false_if_newer_version(self):
         url = 'https://www.mozilla.org/en-US/security/known-vulnerabilities/firefox/'
         self.mocked.get(url, status=200, body='<html data-latest-firefox="52.0.2"></html>')
 
         received = await security_advisories_published('firefox', '54.0')
         assert received is False
 
-    async def test_security_advisory_tasks_returns_error_if_error(self):
+    async def test_security_advisories_tasks_returns_error_if_error(self):
         url = 'https://www.mozilla.org/en-US/security/known-vulnerabilities/firefox/'
         self.mocked.get(url, status=404)
 
         with pytest.raises(TaskError) as excinfo:
             await security_advisories_published('firefox', '54.0')
         assert str(excinfo.value) == 'Security advisories page not available  (404)'
+
+    async def test_product_details_tasks_returns_true_if_present(self):
+        url = 'https://product-details.mozilla.org/1.0/firefox.json'
+        self.mocked.get(url, status=200, body=json.dumps({"releases": {"firefox-52.0": {}}}))
+
+        received = await product_details_published('firefox', '52.0')
+        assert received is True
+
+    async def test_product_details_tasks_returns_false_if_absent(self):
+        url = 'https://product-details.mozilla.org/1.0/firefox.json'
+        self.mocked.get(url, status=200, body=json.dumps({"releases": {"firefox-52.0": {}}}))
+
+        received = await product_details_published('firefox', '54.0')
+        assert received is False
+
+    async def test_product_details_tasks_returns_error_if_error(self):
+        url = 'https://product-details.mozilla.org/1.0/firefox.json'
+        self.mocked.get(url, status=404)
+
+        with pytest.raises(TaskError) as excinfo:
+            await product_details_published('firefox', '54.0')
+        assert str(excinfo.value) == 'Product Details info not available  (404)'
