6ff72e86938cbfa0bd4df324976cad9905629f3f
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-08-07 14:48:52 +0200

    @magopian review.

--- tests/test_views.py
+++ tests/test_views.py
@@ -13,59 +13,61 @@ def cli(loop, test_client):
     return loop.run_until_complete(test_client(get_app(loop=loop)))
 
 
+async def check_response(cli, url, *, status=200, body=None, method="get", **kwargs):
+    resp = await getattr(cli, method)(url, **kwargs)
+    assert resp.status == status
+    if body is not None:
+        assert await resp.json() == body
+    return resp
+
+
 async def test_home_redirects_to_v1(cli):
-    resp = await cli.get("/", allow_redirects=False)
-    assert resp.status == 302
+    resp = await check_response(cli, "/", status=302, allow_redirects=False)
+    assert resp.headers['Location'] == "/v1/"
+
+
+async def test_v1_redirects_to_v1_slash(cli):
+    resp = await check_response(cli, "/v1", status=302, allow_redirects=False)
+    assert resp.headers['Location'] == "/v1/"
 
 
 async def test_oas_spec(cli):
     with open(os.path.join(HERE, "..", "pollbot", "api.yaml"), 'r') as stream:
         oas_spec = yaml.load(stream)
-    resp = await cli.get("/v1/__api__")
-    assert await resp.json() == oas_spec
+    await check_response(cli, "/v1/__api__", body=oas_spec)
 
 
 async def test_home_body(cli):
-    resp = await cli.get("/v1/")
-    assert resp.status == 200
-    assert await resp.json() == {
+    await check_response(cli, "/v1/", body={
         "project_name": "pollbot",
         "project_version": pollbot_version,
         "url": "https://github.com/mozilla/PollBot",
         "http_api_version": HTTP_API_VERSION
-    }
+    })
 
 
 # This is currently a functional test.
 async def test_release_archive(cli):
-    resp = await cli.get("/v1/firefox/54.0/archive")
-    assert resp.status == 200
-    assert await resp.json() == {
+    await check_response(cli, "/v1/firefox/54.0/archive", body={
         "status": "exists"
-    }
+    })
 
 
 async def test_release_archive_404(cli):
-    resp = await cli.get("/v1/thunderbird/54.0/archive")
-    assert resp.status == 404
-    assert await resp.json() == {
+    await check_response(cli, "/v1/invalid-product/54.0/archive", status=404, body={
         "status": 404,
-        "message": "Invalid product: thunderbird not in ['firefox']"
-    }
+        "message": "Invalid product: invalid-product not in ['firefox']"
+    })
 
 
 async def test_release_bedrock_release_notes(cli):
-    resp = await cli.get("/v1/firefox/54.0/bedrock/release-notes")
-    assert resp.status == 200
-    assert await resp.json() == {
+    await check_response(cli, "/v1/firefox/54.0/bedrock/release-notes", body={
         "status": "exists"
-    }
+    })
 
 
 async def test_release_bedrock_release_notes_404(cli):
-    resp = await cli.get("/v1/thunderbird/54.0/bedrock/release-notes")
-    assert resp.status == 404
-    assert await resp.json() == {
+    await check_response(cli, "/v1/invalid-product/54.0/bedrock/release-notes", status=404, body={
         "status": 404,
-        "message": "Invalid product: thunderbird not in ['firefox']"
-    }
+        "message": "Invalid product: invalid-product not in ['firefox']"
+    })
