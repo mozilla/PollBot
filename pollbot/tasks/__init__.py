7f428cc8f56f806fdba1d9ae345f3dc6ff7c9e60
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-08-08 11:57:42 +0200

    @magopian review.

--- pollbot/tasks/__init__.py
+++ pollbot/tasks/__init__.py
@@ -6,12 +6,9 @@ import aiohttp
 from pollbot import __version__ as pollbot_version
 
 
-# Module version, as defined in PEP-0396.
-
-
 def get_session():
     aiohttp_version = pkg_resources.get_distribution("aiohttp").version
-    python_version = '.'.join(map(str, sys.version_info[:3]))
+    python_version = '.'.join([str(v) for v in sys.version_info[:3]])
     return aiohttp.ClientSession(headers={
         "User-Agent": "PollBot/{} aiohttp/{} python/{}".format(
             pollbot_version, aiohttp_version, python_version)
