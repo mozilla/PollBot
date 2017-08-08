4c1dd489a6c33b2ebf9fc4c8f3969bd59212be0c
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-08-08 16:34:02 +0200

    Use Nginx information about the scheme.

--- pollbot/views/home.py
+++ pollbot/views/home.py
@@ -8,11 +8,12 @@ async def redirect(request):
 
 
 async def index(request):
+    proto = request.headers.get('X-Forwarded-Proto', 'http')
     host = request.headers['Host']
     return web.json_response({
         "project_name": "pollbot",
         "project_version": pollbot_version,
         "url": "https://github.com/mozilla/PollBot",
         "http_api_version": HTTP_API_VERSION,
-        "docs": "http://{}/v1/api/doc".format(host)
+        "docs": "{}://{}/v1/api/doc".format(proto, host)
     })
