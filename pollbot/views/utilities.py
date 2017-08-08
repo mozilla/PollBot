071983b107ad5b8441f5098381c75bcd9579e60b
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-08-08 14:49:29 +0200

    Add __version__ endpoint. Fixes #35

--- pollbot/views/utilities.py
+++ pollbot/views/utilities.py
@@ -1,5 +1,7 @@
 import asyncio
+import json
 import os.path
+from contextlib import suppress
 
 import ruamel.yaml as yaml
 from aiohttp import web
@@ -8,6 +10,14 @@ from pollbot.tasks import archives, bedrock, product_details
 
 
 HERE = os.path.dirname(__file__)
+
+
+async def version(request):
+    # Use the version.json file in the current dir.
+    with suppress(IOError):
+        with open("version.json") as fd:
+            return web.json_response(json.load(fd))
+    return web.HTTPNotFound()
 
 
 def render_yaml_file(filename):
