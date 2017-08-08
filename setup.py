89e4e32b37fcd6ff4da48970eaa9304447258946
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-08-08 16:21:22 +0200

    Back to development: 0.2.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ ENTRY_POINTS = {
 
 
 setup(name='pollbot',
-      version='0.1.0',
+      version='0.2.0.dev0',
       description='A service that polls other services about releases deliveries.',
       long_description="{}\n\n{}\n\n{}".format(README, CHANGELOG, CONTRIBUTORS),
       license='Mozilla Public License 2.0',
