c3b6875ae544ce2d0871e2c2bce553fe2a22c49c
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-08-08 11:54:15 +0200

    Add a Dockerfile

--- pollbot/__main__.py
+++ pollbot/__main__.py
@@ -1,6 +1,9 @@
+import os
 from aiohttp import web
 from .app import get_app
 
+PORT = int(os.getenv("PORT", 8000))
+
 
 def main():
-    web.run_app(get_app(), port=8000)
+    web.run_app(get_app(), port=PORT)
