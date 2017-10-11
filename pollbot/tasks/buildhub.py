871ea4cd2313469712b2234ce96e0309bc86092c
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-10-11 18:01:33 +0200

    Add a task to grab a list of build IDs for a release using Buildhub.

--- pollbot/tasks/buildhub.py
+++ pollbot/tasks/buildhub.py
@@ -1,5 +1,6 @@
-from urllib.parse import urlencode
+import json
 
+from pollbot.exceptions import TaskError
 from pollbot.utils import Channel, Status, get_version_channel, yesterday
 
 from . import get_session, build_task_response, heartbeat_factory
@@ -8,13 +9,53 @@ from . import get_session, build_task_response, heartbeat_factory
 BUILDHUB_SERVER = "https://buildhub.stage.mozaws.net/v1"
 
 
-async def buildhub(product, version):
-    params = {
-        "source.product": product,
-        "target.version": '"%s"' % version,
-        "has_build.id": "true",
-        "_limit": 1,
+async def get_build_ids_for_version(product, version, *, size=10):
+    channel = get_version_channel(version)
+    query = {
+        "aggs": {
+            "by_version": {
+                "terms": {
+                    "field": "build.id",
+                    "size": size,
+                    "order": {
+                        "_term": "desc"
+                    }
+                }
+            }
+        },
+        "query": {
+            "bool": {
+                "filter": [
+                    {
+                        "term": {
+                            "target.channel": channel.value.lower()
+                        }
+                    }, {
+                        "term": {
+                            "source.product": product
+                        }
+                    }, {
+                        "term": {
+                            "target.version": version
+                        }
+                    }
+                ]
+            }
+        },
+        "size": 0
     }
+    with get_session() as session:
+        url = '{}/buckets/build-hub/collections/releases/search'
+        url = url.format(BUILDHUB_SERVER)
+        async with session.post(url, data=json.dumps(query)) as response:
+            data = await response.json()
+        build_ids = [r['key'] for r in data['aggregations']['by_version']['buckets']]
+
+        if not build_ids:
+            message = "Couldn't find any build matching."
+            raise TaskError(message, url=url)
+
+        return build_ids
 
     exists_message = 'Build id is {record[build][id]} for this release.'
     missing_message = 'Buildhub does not contain any information about this release yet.'
