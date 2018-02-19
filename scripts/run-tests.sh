14b917322898f4dc95759e4df86d43ea794177b4
Rémy HUBSCHER <rhubscher@mozilla.com>
2018-02-19 17:57:42 +0100

    Add another test for the ValueError case + Fix CircleCI command.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Run tests in the docker image
+export TELEMETRY_USER_ID=502
 python3 -m venv /tmp/tests
 /tmp/tests/bin/pip install -r /app/dev-requirements.txt
 /tmp/tests/bin/pip install /app
--- tests/test_app.py
+++ tests/test_app.py
@@ -14,3 +14,16 @@ def test_get_app_fails_if_telemetry_user_id_is_not_defined():
         pytest.fail("get_app did not raise")
     finally:
         os.environ["TELEMETRY_USER_ID"] = initial_telemetry_user_value
+
+
+def test_get_app_fails_if_telemetry_user_id_is_not_a_integer():
+    initial_telemetry_user_value = os.getenv("TELEMETRY_USER_ID")
+    try:
+        os.environ["TELEMETRY_USER_ID"] = "unknown"
+        get_app()
+    except SystemExit as e:
+        assert e.code == 1
+    else:
+        pytest.fail("get_app did not raise")
+    finally:
+        os.environ["TELEMETRY_USER_ID"] = initial_telemetry_user_value
