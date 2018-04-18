9217fe5b7828fd5f5eaa501c51555848a5a4a687
Philip Cristiano <git@philipcristiano.com>
2018-04-18 09:08:27 -0400

    Flake8: Run as part of tests

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -4,4 +4,5 @@ export TELEMETRY_USER_ID=502
 python3 -m venv /tmp/tests
 /tmp/tests/bin/pip install -r /app/dev-requirements.txt
 /tmp/tests/bin/pip install /app
+/tmp/tests/bin/flake8 pollbot tests
 /tmp/tests/bin/py.test /app/tests
