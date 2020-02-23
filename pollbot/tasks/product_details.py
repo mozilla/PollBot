f6b627b91f773d91d71f543129353888185ee445
Rob Lemley <rob@thunderbird.net>
2020-02-22 22:00:32 -0500

    Support Thunderbird in product_details task.

--- pollbot/tasks/product_details.py
+++ pollbot/tasks/product_details.py
@@ -2,22 +2,61 @@ from pollbot.exceptions import TaskError
 from pollbot.utils import Channel, get_version_channel, build_version_id, Status
 from . import get_session, heartbeat_factory, build_task_response
 
+_product_details = {
+    "thunderbird": {
+        "versions_url": "thunderbird_versions.json",
+        "releases_url": "thunderbird.json",
+        "ongoing_versions": {
+            "release": "LATEST_THUNDERBIRD_VERSION",
+            "beta": "LATEST_THUNDERBIRD_DEVEL_VERSION",
+            "nightly": "LATEST_THUNDERBIRD_NIGHTLY_VERSION"
+        },
+    },
+    "firefox": {
+        "versions_url": "firefox_versions.json",
+        "releases_url": "firefox.json",
+        "ongoing_versions": {
+            "esr": "FIREFOX_ESR",
+            "release": "LATEST_FIREFOX_VERSION",
+            "beta": "LATEST_FIREFOX_DEVEL_VERSION",
+            "nightly": "FIREFOX_NIGHTLY",
+            "devedition": "FIREFOX_DEVEDITION",
+        },
+    },
+    "devedition": {
+        "versions_url": "firefox_versions.json",
+        "releases_url": "firefox.json",
+        "ongoing_versions": {
+            "devedition": "FIREFOX_DEVEDITION",
+        },
+    },
+}
+
+
+def details_versions_url(product):
+    return _product_details[product]['versions_url']
+
+
+def details_releases_url(product):
+    return _product_details[product]['releases_url']
+
+
+def details_ongoing_versions(product, body):
+    rv = {}
+    for channel, version_key in _product_details[product]['ongoing_versions'].items():
+        rv[channel] = body.get(version_key)
+    return rv
+
 
 async def ongoing_versions(product):
     async with get_session() as session:
-        url = 'https://product-details.mozilla.org/1.0/firefox_versions.json'
+        url = 'https://product-details.mozilla.org/1.0/{}'.format(details_versions_url(product))
         async with session.get(url) as resp:
             if resp.status != 200:
                 msg = 'Product Details info not available (HTTP {})'.format(resp.status)
                 raise TaskError(msg, url=url)
             body = await resp.json()
-            return {
-                "esr": body["FIREFOX_ESR"],
-                "release": body["LATEST_FIREFOX_VERSION"],
-                "beta": body["LATEST_FIREFOX_DEVEL_VERSION"],
-                "nightly": body["FIREFOX_NIGHTLY"],
-                "devedition": body["FIREFOX_DEVEDITION"],
-            }
+            return details_ongoing_versions(product, body)
 
 
 async def product_details(product, version):
@@ -25,17 +64,18 @@ async def product_details(product, version):
         versions = await ongoing_versions(product)
         status = build_version_id(versions["nightly"]) >= build_version_id(version)
         message = "Last nightly version is {}".format(versions["nightly"])
-        url = "https://product-details.mozilla.org/1.0/{}_versions.json".format(product)
+        url = "https://product-details.mozilla.org/1.0/{}".format(details_versions_url(product))
         return build_task_response(status, url, message)
 
     async with get_session() as session:
-        url = 'https://product-details.mozilla.org/1.0/firefox.json'
+        url = 'https://product-details.mozilla.org/1.0/{}'.format(details_releases_url(product))
         async with session.get(url) as resp:
             if resp.status != 200:
                 msg = 'Product Details info not available (HTTP {})'.format(resp.status)
                 raise TaskError(msg, url=url)
             body = await resp.json()
-            status = 'firefox-{}'.format(version) in body['releases']
+            details_product = 'firefox' if product == 'devedition' else product
+            status = '{}-{}'.format(details_product, version) in body['releases']
 
             exists_message = "We found product-details information about version {}"
             missing_message = "We did not find product-details information about version {}"
