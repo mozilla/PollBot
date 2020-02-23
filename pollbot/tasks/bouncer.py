f978e3d7e5729f5886a0828442152649282824a9
Rob Lemley <rob@thunderbird.net>
2020-02-22 21:47:36 -0500

    Support Thunderbird in bouncer task.

--- pollbot/tasks/bouncer.py
+++ pollbot/tasks/bouncer.py
@@ -14,10 +14,15 @@ async def bouncer(product, version):
 
     if product == 'devedition':
         channel_value = "DEVEDITION"
-    if channel == Channel.RELEASE:
-        product_channel = 'firefox'
-    else:
         product_channel = 'firefox-{}'.format(channel_value.lower())
+    else:  # product is 'firefox' or 'thunderbird'
+        if channel == Channel.RELEASE:
+            product_channel = product
+        elif channel == Channel.BETA:
+            product_channel = '{}-{}'.format(product, channel_value.lower())
+        else:  # No nightly bouncer for Thunderbird
+            return build_task_response(True, "", "No nightly bouncer for Thunderbird.")
+
     url = 'https://download.mozilla.org?product={}-latest-ssl&os=linux64&lang=en-US'.format(
             product_channel)
 
